e2cb87615eff32234309645d3602d0d67401a449
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2026-03-09 22:01:22 +0700

    Add project files.

---
+++ The_Last_Dance_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace The_Last_Dance_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity relationships and constraints here
+        }
+    }
+}
